5f17b805f67bee92123eb1693171e57a548716db
vivekpadelkar <vivek.padelkar@gmail.com>
2024-03-23 17:43:37 +0530

    added code for file upload

--- client/src/pages/Profile.page.jsx
+++ client/src/pages/Profile.page.jsx
@@ -1,12 +1,58 @@
 import { useSelector } from 'react-redux'
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage'
+import { toast } from 'react-toastify'
+import { app } from '../fireBase'
 import PasswordCheckList from '../components/PasswordCheckList.component'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Profile = () => {
+  const fileRef = useRef()
   const { currentUser } = useSelector((state) => state.user)
   const [password, setPassword] = useState('')
   const [retypePassword, setRetypePassword] = useState('')
   const [isValidPassword, setIsValidPassword] = useState(false)
+  const [file, setFile] = useState(undefined)
+  const [filePercentage, setFilePercentage] = useState(0)
+  const [formData, setFormData] = useState({})
+
+  useEffect(() => {
+    if (file) {
+      console.log('useeffect ran')
+      handleFileUpload(file)
+    }
+  }, [file])
+
+  console.log('i am working with' + JSON.stringify(formData))
+  async function handleFileUpload(file) {
+    try {
+      console.log('inside file uploading')
+      const storage = getStorage(app)
+      const filename = new Date().getTime() + file.name
+      const storegeRef = ref(storage, filename)
+      const uploadTask = uploadBytesResumable(storegeRef, file)
+      uploadTask.on('state_changed', (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        console.log(progress)
+        setFilePercentage(Math.round(progress))
+      })
+      await uploadTask
+      const downloadURL = await getDownloadURL(storegeRef)
+      toast.success('Profile picture updated successfully!')
+      // Use the download URL as needed (e.g., update form data)
+      setFormData({
+        ...formData,
+        avatar: downloadURL,
+      })
+    } catch (error) {
+      console.log(error)
+      toast.error('Somthing went wrong, image must be < 2mb !' + error.message)
+    }
+  }
 
   const handleChange = (e) => {
     if (e.target.id === 'password') setPassword(e.target.value)
@@ -45,17 +91,32 @@ const Profile = () => {
       toast.error(error?.response?.data?.message || error)
     }
   }
+
   return (
     <div className="p-3 max-w-lg mx-auto">
+      {/* <img src={LoadingGif} /> */}
+
       <h1 className="font-semibold text-center text-3xl mx-auto my-7">
         Profile
       </h1>
       <form className="flex flex-col gap-3">
+        <input
+          hidden
+          type="file"
+          id="input"
+          onChange={(e) => setFile(e.target.files[0])}
+          ref={fileRef}
+          accept="image/jpeg, image/png, image/jpeg"
+        />
         <img
           className="rounded-full h-24 w-24 cursor-pointer object-cover self-center mt-2"
-          src={currentUser.avatar}
+          src={formData?.avatar || currentUser.avatar}
           alt={currentUser.username}
+          onClick={() => fileRef.current.click()}
         />
+        {filePercentage > 0 && filePercentage < 100 ? (
+          <p className="text-center">Uploading..{filePercentage}%</p>
+        ) : null}
         <input
           className="p-3 border rounded-lg"
           id="username"
