868891ec05381eb94304d5de96d94d15f6831a62
vivekpadelkar <vivek.padelkar@gmail.com>
2024-04-01 18:21:06 +0530

    port changed

--- client/src/utils/axiosConfig.js
+++ client/src/utils/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5002/api/v1',
-  baseURL: 'https://eastside-estate.onrender.com/api/v1',
+  baseURL: 'https://eastside-estate.onrender.com/api/v1/',
   withCredentials: true,
 })
 
